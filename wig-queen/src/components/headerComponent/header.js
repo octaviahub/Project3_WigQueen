0815bbbd69d26301654330e9098cb325e5919098
Octavia Cephas <octavia.cephas@gmail.com>
2018-04-21 13:43:05 -0400

    :

--- wig-queen/src/components/headerComponent/header.js
+++ wig-queen/src/components/headerComponent/header.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 
 
-import Header extends Component {
+class Header extends Component {
 
   render() {
     return (
       <header>
-          WigQueen
+
+         <div className="logo">
+
+            WigQueen Logo
+
+         </div>
+
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Shop</a>
+              </li>
+              <li>
+                <a href="#">Tutorials</a>
+              </li>
+              <li>
+                <a href="#">About WigQueen</a>
+              </li>
+            </ul>
+          </nav>
+
       </header>
     );
   }
---
+++ wig-queen/src/components/pages/homePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Homepage extends Component {
+
+  render() {
+    return (
+      <div className="container-fluid">
+
+        Wig Queen Home Page content
+
+      </div> 
+    );
+  }
+}
+
+export default Homepage;
