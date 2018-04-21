edfb19ff812de17440167521d103ce72d3d996d1
Octavia Cephas <octavia.cephas@gmail.com>
2018-04-21 11:23:55 -0400

    header.js and footer.js edits/creation

---
+++ wig-queen/src/components/footerComponent/footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Footer extends Component {
+
+  render() {
+    return (
+
+      <footer>
+            Copyright &copy; 2018 Octavia Cephas
+      </footer>
+      
+    );
+  }
+}
+
+export default Footer;
