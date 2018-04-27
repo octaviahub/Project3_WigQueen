c1de10c854046360b02be43b74104d6be721c978
Octavia Cephas <octavia.cephas@gmail.com>
2018-04-26 22:21:26 -0400

    attempting nom youtube react

--- wig-queen/src/components/pages/tutorialsPage.js
+++ wig-queen/src/components/pages/tutorialsPage.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
 
 
+
 class Tutorialspage extends Component {
 
   render() {
     return (
       <div className="container-fluid">
 
+      	<div className="container">
 
-        <h1> Hello World </h1>
+      		<div className="row" className="col-sm-8">
 
-      </div> 
+        	<h1 className="tutorials-h1"> Tutorials</h1>
+
+			</div>
+		</div>
+	</div>
+
     );
   }
 }
 
 export default Tutorialspage;
+
+const opts = {
+
+
+	// YOUTUBE NPM REACT 
+
+//   		height: '390',
+//   		width: '640',
+//   		playerVars: { https://www.youtube.com/watch?v=gthxlqcXvBI
+//   		autoplay: 1
+//   	}
+//   };
+
+ 
+//     return (
+//     	<Youtube
+//     	videoId="2g811Eo7K8U"
+//     	opts= {opts}
+//     	onReady= {this._onReady}
+//     	/>
+//     );
+// }
+// _onReady(event) {
+// 	event.target.pauseVideo();
+
+}
+
+//       <div className="container-fluid">
+
+//       	<div className="container">
+
+//       		<div className="row" className="col-sm-8">
+
+//         	<h1 className="tutorials-h1"> Tutorials</h1>
+
+// 			</div>
+// 		</div>
+// 	</div>
+
+//     );
+//   }
+// }
